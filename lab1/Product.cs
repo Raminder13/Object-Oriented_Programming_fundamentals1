1f972eed1d5c4510e345bc2d0cc057e81cdf224c
Raminder Singh <raminder1319@gmail.com>
2023-06-20 08:44:37 -0500

    Add project files.

---
+++ lab1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Code { get; set; }
+
+        public Product(string name, int price, string code)
+        {
+            Name = name;
+            Price = price;
+            Code = code;
+        }
+    }
+}
