845fc104a3ccf853780a4adb76668f47170fb961
Raminder Singh <raminder1319@gmail.com>
2023-06-20 08:52:12 -0500

    updated

--- lab1/Product.cs
+++ lab1/Product.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Code { get; set; }
+
+        public Product(string name, int price, string code)
+        {
+            Name = name;
+            Price = price;
+            Code = code;
+        }
+    }
+}
--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new VendingMachine(1);
+
+            Product coke = new Product("Coke", 2, "A1");
+            Product sprite = new Product("Sprite", 3, "A2");
+            Product fanta = new Product("Fanta", 3, "A3");
+
+            vendingMachine.StockItem(coke, 10);
+            vendingMachine.StockItem(sprite, 10);
+            vendingMachine.StockItem(fanta, 10);
+
+            vendingMachine.StockFloat(1, 10);
+            vendingMachine.StockFloat(2, 10);
+            vendingMachine.StockFloat(5, 10);
+            vendingMachine.StockFloat(10, 10);
+            vendingMachine.StockFloat(20, 10);
+            vendingMachine.StockFloat(50, 10);
+            vendingMachine.StockFloat(100, 10);
+
+            List<int> money = new List<int>();
+
+            money.Add(20);
+            money.Add(10);
+            money.Add(5);
+
+            Console.WriteLine(vendingMachine.VendItem("A1", money));
+        }
+    }
+}
